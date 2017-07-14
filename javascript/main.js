4514e2847a8db128ba9f45ef1c41b0b958c92353
AustinJ <austin.jay.kurtis@gmail.com>
2017-07-14 13:44:19 -0500

    Needs more work

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+function openNav() {
+	document.getElementById("idNav").style.width = "250px";
+	document.getElementById("wrapper").style.marginLeft = "250px";
+}
+
+function closeNav() {
+	document.getElementById("idNav").style.width = "0px";
+	document.getElementById("wrapper").style.marginLeft = "0px";
+}
+
+function openBio() {
+	documnet.getElementById().style.width = "350px";
+	documnet.getElementById().style.width = "910px";
+}
+
